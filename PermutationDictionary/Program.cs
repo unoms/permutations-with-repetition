d318caf691dc1a2e9426d02a1dc095ed158a13cc
Serge Balakin <balakin@unom.ru>
2021-05-13 19:35:46 +0300

    Добавьте файлы проекта.

---
+++ PermutationDictionary/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermutationDictionary
+{
+    class Program
+    {
+        static void Permutation(int[] dictionary, int[] places, int position = 0)
+        {
+            if(places.Length == position)
+            {
+                //Print permutation
+                for (int i = 0; i < places.Length; i++)
+                    Console.Write(places[i]);
+                Console.WriteLine();
+                return;
+            }
+            
+            //insert from dictionary
+            for(int i = 0; i < dictionary.Length; i++)
+            {
+                places[position] = dictionary[i];
+                Permutation(dictionary, places, position + 1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] dic = new int[] { 1, 2, 3};
+            Permutation(dic, new int[3]);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Permutations with repetition
+
+We have a dictionary (numbers in this realisation) and Permutation() creates permutations on the given number of positions (int[] places).
+
+If the length of the dictionary equals the number of places, then the number of permutations equals the length of the dictionary in the power of the length
